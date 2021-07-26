16f591032a16790f5b7a756fcfa85a0c34a891ba
feb day <drons_dron@mail.ru>
2021-07-27 01:20:31 +0400

    create converting audio

--- api_trello.py
+++ api_trello.py
@@ -1,19 +1,12 @@
-import requests, datetime
+import requests, datetime, os
 
-# Артур ключи
-# query = {
-# 	"key":"c10a9b515c27095e719526ce58ea50b8",
-# 	"token":"978e94d25b3e97518268c76f63d0151a9e948c39f0c4be1056dfaf3e3c6bdc34"
-# }
-
-# Мои ключи
 query = {
-	"key":"96c960faf9c0c2a49d4dd761fb62c590",
-	"token":"17f8826c941fc48c54a837da43dc5555b8badb75b8267e2271edca7d5dfb4de5"
+	"key": os.environ["APIkey"],
+	"token": os.environ["APItoken"]
 }
 
 
-def getIdList(IdBoard: str = "60c78ce8498d4f195c7c51d2"):
+def getIdList(IdBoard):
 	return requests.get(f"https://trello.com/1/boards/{IdBoard}/lists", params=query).json()
 
 def newCard(name, IdList, date_start = None, date_last = None, description = None, members = None):
@@ -30,21 +23,3 @@ def addMembers(IdCard, value):
 
 def updateCard(IdCard, date_start = None, date_last = None, description = None, members = None):
 	return requests.put(f"https://trello.com/1/cards/{IdCard}", params=query, data={"due":date_last, "start":date_start, "idMembers":members, "desc":description}).json()
-
-if __name__ == '__main__':
-	now = datetime.datetime.now()
-	members_username = {
-		"Иванов": "feb62",
-		"Эпитов": 0,
-		"Малютин": 0,
-	}
-
-	idList = getIdList(IdBoard = "60d0646f90d48645032c4820")
-	members = getMembers(IdBoard = "60d0646f90d48645032c4820")
-	id_user = [i["id"] for i in members if i["username"] == members_username["Иванов"]]
-	card = newCard(name="Ещё одна карточка, хаха", IdList=idList[0]["id"], date_start=now, date_last=now+datetime.timedelta(days=5), members=[ id_user ])
-	print(card["shortUrl"])
-	# idCard = getIdCard(IdList = idList[0]["id"])
-	# ans_member = addMembers(IdCard = idCard[0]["id"], value = members[0]["id"])
-	# a = updateCard(IdCard = idCard[0]["id"], description="Какая-то задача на день")
-	
--- voice.py
+++ voice.py
@@ -1,12 +1,27 @@
 import speech_recognition as sr
+from pydub import AudioSegment
+import os
 
 def voice_to_text(name_file):
 	recog = sr.Recognizer()
-	sample_audio = sr.AudioFile(name_file)
-	with sample_audio as audio_file:
-		audio_content = recog.record(audio_file)
-	text = recog.recognize_google(audio_content, language = "ru-RU")
-	return text
+	try:
+		sample_audio = sr.AudioFile(name_file)
+		with sample_audio as audio_file:
+			audio_content = recog.record(audio_file)
+		text = recog.recognize_google(audio_content, language = "ru-RU")
+		return text, name_file
+	except ValueError as e:
+		_file = name_file.split('.')
+		if _file[1] == "mp3":
+			sound = AudioSegment.from_mp3(file=name_file)
+		elif _file[1] == "flv":
+			sound = AudioSegment.from_flv(file=name_file)
+		elif _file[1] == "ogg":
+			sound = AudioSegment.from_ogg(file=name_file)
+		elif _file[1] == "raw":
+			sound = AudioSegment.from_raw(file=name_file)
 
-if __name__ == '__main__':
-	print(voice_to_text("test.wav"))
+		os.remove(name_file) # удаление временного аудио-файла
+		sound.export(_file[0]+".wav", format="wav")
+		return voice_to_text(_file[0]+".wav")
+
