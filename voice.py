a1d3fe7da2d870dbb1d12158a9fd5ba527a847c5
feb day <drons_dron@mail.ru>
2021-07-28 23:07:45 +0400

    upd

--- voice.py
+++ voice.py
@@ -1,38 +1,9 @@
-# import speech_recognition as sr
-# from pydub import AudioSegment
-# import os
-
-# def voice_to_text(name_file):
-# 	recog = sr.Recognizer()
-# 	try:
-# 		sample_audio = sr.AudioFile(name_file)
-# 		with sample_audio as audio_file:
-# 			audio_content = recog.record(audio_file)
-# 		text = recog.recognize_google(audio_content, language = "ru-RU")
-# 		return text, name_file
-# 	except ValueError as e:
-# 		_file = name_file.split('.')
-# 		if _file[1] == "mp3":
-# 			sound = AudioSegment.from_mp3(file=name_file)
-# 		elif _file[1] == "flv":
-# 			sound = AudioSegment.from_flv(file=name_file)
-# 		elif _file[1] == "ogg":
-# 			sound = AudioSegment.from_ogg(file=name_file)
-# 		elif _file[1] == "raw":
-# 			sound = AudioSegment.from_raw(file=name_file)
-
-# 		os.remove(name_file) # удаление временного аудио-файла
-# 		sound.export(_file[0]+".wav", format="wav")
-# 		return voice_to_text(_file[0]+".wav")
-
 import speech_recognition as sr
 import subprocess, os
 
 def convert_audio(audio_input, audio_output):
 	cmds = ['ffmpeg', '-i', audio_input, audio_output]
 	os.system(f"ffmpeg -i {audio_input} {audio_output}")
-	# p = subprocess.Popen(cmds)
-	# p.terminate()
 	return True
 
 def voice_to_text(name_file):
